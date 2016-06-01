543c8a1750adeff481df38ea5917d6b6ebf18a2f
David Zmick <dpzmick@gmail.com>
2016-06-01 00:00:35 -0500

    it worked

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![feature(unboxed_closures)]
 extern crate image;
+extern crate llvm;
 
 use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
 use std::fmt::Debug;
+
+use llvm::Compile;
+use llvm::ExecutionEngine;
+use llvm::GetContext;
 
 use std::fs::File;
 use std::path::Path;
@@ -16,10 +21,19 @@ trait Inputable {
     fn at(&self, x: i64, y: i64) -> i64;
     fn width(&self) -> i64;
     fn height(&self) -> i64;
+    fn compile<'a>(&self, module: &'a llvm::CSemiBox<'a, llvm::Module>) -> &'a llvm::Function;
 }
 
-trait Eval: Debug {
+trait Expr: Debug {
     fn eval(&self, env: FunctionEnv, inputs: &Vec<&Inputable>) -> i64;
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   context: &'a llvm::Context,
+                   module: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   builder: &'a llvm::Builder,
+                   inputs: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value;
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -53,6 +67,12 @@ impl Inputable for VecInpt {
     fn height(&self) -> i64 {
         0
     }
+
+    fn compile<'a>(&self, module: &'a llvm::CSemiBox<'a, llvm::Module>) -> &'a llvm::Function {
+        let ft = llvm::Type::get::<fn(i64, i64) -> i64>(module.get_context());
+
+        module.add_function("vec_input", ft)
+    }
 }
 
 impl Inputable for ImageBuffer<Luma<u8>, Vec<u8>> {
@@ -76,15 +96,60 @@ impl Inputable for ImageBuffer<Luma<u8>, Vec<u8>> {
     fn height(&self) -> i64 {
         self.height() as i64
     }
+
+    fn compile<'a>(&self, module: &'a llvm::CSemiBox<'a, llvm::Module>) -> &'a llvm::Function {
+        let context = module.get_context();
+
+        let ft = llvm::Type::get::<fn(i64, i64) -> i64>(module.get_context());
+        let f = module.add_function("image_buffer", ft);
+        f.add_attribute(llvm::Attribute::AlwaysInline);
+        let entry = f.append("entry");
+
+        let builder = llvm::Builder::new(&context);
+
+        builder.position_at_end(entry);
+
+        let x = &f[0];
+        let y = &f[1];
+        let w = (self.width() as i64).compile(&context);
+
+        let offset = builder.build_mul(w, y);
+        let offset = builder.build_add(offset, x);
+
+        let ty0 = llvm::Type::get::<i8>(&context);
+        let ty1 = llvm::Type::new_pointer(ty0);
+
+        let ptr = (self.as_ptr() as u64).compile(&context);
+        let ptr = builder.build_int_to_ptr(ptr, ty1);
+        let ptr = builder.build_gep(ptr, &[offset]);
+        let e = builder.build_load(ptr);
+
+        let e = builder.build_zext(e, llvm::Type::get::<i64>(&context));
+        builder.build_ret(e);
+
+        f
+    }
 }
 
 
 #[derive(Debug, Clone, Copy)]
 struct ConstExpr { v: i64 }
 
-impl Eval for ConstExpr {
+impl Expr for ConstExpr {
     fn eval(&self, _: FunctionEnv, _: &Vec<&Inputable>) -> i64 {
         self.v
+    }
+
+    fn compile<'a>(&self,
+                   _: &'a llvm::Value,
+                   _: &'a llvm::Value,
+                   context: &'a llvm::Context,
+                   _: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   _: &'a llvm::Builder,
+                   _: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        self.v.compile(context)
     }
 }
 
@@ -117,11 +182,26 @@ fn test_const() {
 #[derive(Debug, Clone, Copy)]
 struct VarRef { v: Var }
 
-impl Eval for VarRef {
+impl Expr for VarRef {
     fn eval(&self, env: FunctionEnv, _: &Vec<&Inputable>) -> i64 {
         match self.v {
             Var::X => env.x,
             Var::Y => env.y
+        }
+    }
+
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   _: &'a llvm::Context,
+                   _: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   _: &'a llvm::Builder,
+                   _: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        match self.v {
+            Var::X => x,
+            Var::Y => y
         }
     }
 }
@@ -163,14 +243,28 @@ fn test_var_ref() {
 }
 
 #[derive(Debug)]
-struct AddExpr{ e1: Box<Eval>, e2: Box<Eval> }
+struct AddExpr{ e1: Box<Expr>, e2: Box<Expr> }
 
-impl Eval for AddExpr {
+impl Expr for AddExpr {
     fn eval(&self, env: FunctionEnv, inpt: &Vec<&Inputable>) -> i64 {
         let e1 = self.e1.eval(env.clone(), inpt);
         let e2 = self.e2.eval(env.clone(), inpt);
 
         e1 + e2
+    }
+
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   context: &'a llvm::Context,
+                   module: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   builder: &'a llvm::Builder,
+                   inputs: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        let v1 = self.e1.compile(x, y, context, module, builder, inputs);
+        let v2 = self.e2.compile(x, y, context, module, builder, inputs);
+        builder.build_add(v1, v2)
     }
 }
 
@@ -212,14 +306,28 @@ fn test_add_expr() {
 }
 
 #[derive(Debug)]
-struct MulExpr{ e1: Box<Eval>, e2: Box<Eval> }
+struct MulExpr{ e1: Box<Expr>, e2: Box<Expr> }
 
-impl Eval for MulExpr {
+impl Expr for MulExpr {
     fn eval(&self, env: FunctionEnv, inpt: &Vec<&Inputable>) -> i64 {
         let e1 = self.e1.eval(env.clone(), inpt);
         let e2 = self.e2.eval(env.clone(), inpt);
 
         e1 * e2
+    }
+
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   context: &'a llvm::Context,
+                   module: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   builder: &'a llvm::Builder,
+                   inputs: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        let v1 = self.e1.compile(x, y, context, module, builder, inputs);
+        let v2 = self.e2.compile(x, y, context, module, builder, inputs);
+        builder.build_mul(v1, v2)
     }
 }
 
@@ -252,12 +360,60 @@ fn test_mul_expr() {
 }
 
 #[derive(Debug)]
-struct SqrtExpr { x: Box<Eval> }
+struct SqrtExpr { x: Box<Expr> }
 
-impl Eval for SqrtExpr {
+impl Expr for SqrtExpr {
     fn eval(&self, env: FunctionEnv, inpt: &Vec<&Inputable>) -> i64 {
         let x = self.x.eval(env, inpt);
         (x as f64).sqrt() as i64
+    }
+
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   _: &'a llvm::Context,
+                   module: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   builder: &'a llvm::Builder,
+                   inputs: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        let context = module.get_context();
+
+        let ftype = llvm::Type::get::<fn(i64) -> (i64)>(context);
+        let f = module.add_function("sqrt", ftype);
+        f.add_attribute(llvm::Attribute::AlwaysInline);
+        let entry     = f.append("entry");
+        let loop_cond = f.append("loop_cond");
+        let loop_body = f.append("loop_body");
+        let exit      = f.append("exit");
+
+        let builder2 = llvm::Builder::new(&context);
+
+        builder2.position_at_end(entry);
+        let tmp = builder2.build_alloca(llvm::Type::get::<i64>(context));
+        let one = 1i64.compile(context);
+        builder2.build_store(one, tmp);
+        builder2.build_br(loop_cond);
+
+        builder2.position_at_end(loop_cond);
+        let val = builder2.build_load(tmp);
+        let val = builder2.build_mul(val, val);
+        let cmp = builder2.build_cmp(val, &f[0], llvm::Predicate::LessThan);
+        builder2.build_cond_br(cmp, loop_body, Some(exit));
+
+        builder2.position_at_end(loop_body);
+        let val = builder2.build_load(tmp);
+        let inc = builder2.build_add(val, 1i64.compile(&context));
+        builder2.build_store(inc, tmp);
+        builder2.build_br(loop_cond);
+
+        builder2.position_at_end(exit);
+        let val = builder2.build_load(tmp);
+        builder2.build_ret(val);
+
+        // expression
+        let v = self.x.compile(x, y, context, module, builder, inputs);
+        builder.build_call(f, &[v])
     }
 }
 
@@ -272,14 +428,28 @@ fn test_sqrt_expr() {
 }
 
 #[derive(Debug)]
-struct InputExpr { id: usize, x: Box<Eval>, y: Box<Eval> }
+struct InputExpr { id: usize, x: Box<Expr>, y: Box<Expr> }
 
-impl Eval for InputExpr {
+impl Expr for InputExpr {
     fn eval(&self, env: FunctionEnv, inpt: &Vec<&Inputable>) -> i64 {
         let x = self.x.eval(env.clone(), inpt);
         let y = self.y.eval(env.clone(), inpt);
 
         inpt[self.id].at(x, y)
+    }
+
+    fn compile<'a>(&self,
+                   x: &'a llvm::Value,
+                   y: &'a llvm::Value,
+                   context: &'a llvm::Context,
+                   module: &'a llvm::CSemiBox<'a, llvm::Module>,
+                   builder: &'a llvm::Builder,
+                   inputs: &Vec<&'a llvm::Function>)
+        -> &'a llvm::Value
+    {
+        let x = self.x.compile(x, y, context, module, builder, inputs);
+        let y = self.y.compile(x, y, context, module, builder, inputs);
+        builder.build_call(inputs[self.id], &[x, y])
     }
 }
 
@@ -316,7 +486,7 @@ fn test_inpt_expr() {
 
 struct Function1 {
     input: Box<Inputable>,
-    e:     Box<Eval>
+    e:     Box<Expr>
 }
 
 impl Inputable for Function1 {
@@ -341,11 +511,31 @@ impl Inputable for Function1 {
     fn height(&self) -> i64 {
         self.input.height()
     }
+
+    fn compile<'a>(&self, module: &'a llvm::CSemiBox<'a, llvm::Module>) -> &'a llvm::Function {
+        // compile all the inputs
+        let input = self.input.compile(&module);
+
+        let context = module.get_context();
+
+        let ftype = llvm::Type::get::<fn(i64, i64) -> (i64)>(&context);
+        let f = module.add_function("function1", ftype);
+        f.add_attribute(llvm::Attribute::AlwaysInline);
+        let entry = f.append("entry");
+
+        let builder = llvm::Builder::new(&context);
+
+        builder.position_at_end(entry);
+        let e = self.e.compile(&f[0], &f[1], &context, module, &builder, &vec![input]);
+        builder.build_ret(e);
+
+        f
+    }
 }
 
 impl Function1 {
     pub fn new<F>(inpt: Box<Inputable>, gen: F) -> Self
-        where F: Fn(VarRef, VarRef, &Fn(Box<Eval>, Box<Eval>) -> InputExpr) -> Box<Eval>
+        where F: Fn(VarRef, VarRef, &Fn(Box<Expr>, Box<Expr>) -> InputExpr) -> Box<Expr>
     {
         let x = VarRef {v: Var::X};
         let y = VarRef {v: Var::Y};
@@ -398,7 +588,7 @@ impl Function1 {
 struct Function2 {
     input1: Box<Inputable>,
     input2: Box<Inputable>,
-    e:      Box<Eval>
+    e:      Box<Expr>
 }
 
 impl Inputable for Function2 {
@@ -423,6 +613,28 @@ impl Inputable for Function2 {
     fn height(&self) -> i64 {
         self.input1.height()
     }
+
+    fn compile<'a>(&self, module: &'a llvm::CSemiBox<'a, llvm::Module>) -> &'a llvm::Function {
+        // compile all the inputs
+        let input1 = self.input1.compile(&module);
+        let input2 = self.input2.compile(&module);
+
+        let context = module.get_context();
+
+        let ftype = llvm::Type::get::<fn(i64, i64) -> (i64)>(&context);
+        let f = module.add_function("function2", ftype);
+        f.add_attribute(llvm::Attribute::AlwaysInline);
+        let entry = f.append("entry");
+
+        let builder = llvm::Builder::new(&context);
+
+        builder.position_at_end(entry);
+        let e = self.e.compile(&f[0], &f[1], &context, module, &builder, &vec![input1, input2]);
+        builder.build_ret(e);
+
+        f
+    }
+
 }
 
 impl Function2 {
@@ -430,9 +642,9 @@ impl Function2 {
         where F: Fn(
             VarRef,
             VarRef,
-            &Fn(Box<Eval>, Box<Eval>) -> InputExpr,
-            &Fn(Box<Eval>, Box<Eval>) -> InputExpr)
-        -> Box<Eval>
+            &Fn(Box<Expr>, Box<Expr>) -> InputExpr,
+            &Fn(Box<Expr>, Box<Expr>) -> InputExpr)
+        -> Box<Expr>
     {
         let x = VarRef {v: Var::X};
         let y = VarRef {v: Var::Y};
@@ -467,6 +679,154 @@ impl Function2 {
 
         out
     }
+}
+
+fn run_jit<I: Inputable>(inpt: &I) -> ImageBuffer<Luma<u8>, Vec<u8>> {
+    let xbound = inpt.width();
+    let ybound = inpt.height();
+
+    // this is actually mutated but don't tell anyone
+    let out = ImageBuffer::new(xbound as u32, ybound as u32);
+
+    let context = unsafe { llvm::Context::get_global() };
+    let m = llvm::Module::new("jitmodule", &context);
+
+    {
+        let e = inpt.compile(&m);
+
+        let ft = llvm::Type::get::<fn() -> ()>(&context);
+        let f = m.add_function("jitfunction", ft);
+        let entry       = f.append("entry");
+        let ilooph      = f.append("iloop_header");
+        let iloopc      = f.append("iloop_cond");
+        let iloopb      = f.append("iloop_body");
+        let jlooph      = f.append("jloop_header");
+        let jloopc      = f.append("jloop_cond");
+        let jloopb      = f.append("jloop_body");
+        let br_big      = f.append("clamp_ifbigtrue");
+        let br_nobig    = f.append("clamp_ifbigfalse");
+        let br_little   = f.append("clamp_iflittletrue");
+        let br_nolittle = f.append("clamp_iflittlefalse");
+        let br_done     = f.append("clamp_done");
+        let jloope      = f.append("jloop_end");
+        let iloope      = f.append("iloop_end");
+        let exit        = f.append("exit");
+
+        let builder = llvm::Builder::new(&context);
+
+        builder.position_at_end(entry);
+        let i = builder.build_alloca(llvm::Type::get::<i64>(&context));
+        let j = builder.build_alloca(llvm::Type::get::<i64>(&context));
+        builder.build_br(ilooph);
+
+        builder.position_at_end(ilooph);
+        builder.build_store(0i64.compile(&context), i);
+        builder.build_br(iloopc);
+
+        builder.position_at_end(iloopc);
+        let ibound = inpt.width().compile(&context);
+        let ival = builder.build_load(i);
+        let cmp = builder.build_cmp(ival, ibound, llvm::Predicate::LessThan);
+        builder.build_cond_br(cmp, iloopb, Some(exit));
+
+        builder.position_at_end(iloopb);
+        builder.build_br(jlooph);
+
+        builder.position_at_end(jlooph);
+        builder.build_store(0i64.compile(&context), j);
+        builder.build_br(jloopc);
+
+        builder.position_at_end(jloopc);
+        let jbound = inpt.height().compile(&context);
+        let jval = builder.build_load(j);
+        let cmp = builder.build_cmp(jval, jbound, llvm::Predicate::LessThan);
+        builder.build_cond_br(cmp, jloopb, Some(iloope));
+
+        builder.position_at_end(jloopb);
+        let ival = builder.build_load(i);
+        let jval = builder.build_load(j);
+
+        let px = builder.build_call(e, &[ival, jval]);
+
+        // // clamp the pixel
+        let newpx = builder.build_alloca(llvm::Type::get::<i64>(&context));
+        let zero = 0i64.compile(&context);
+        builder.build_store(zero, newpx);
+
+        let max = 225i64.compile(&context);
+        let cmp = builder.build_cmp(px, max, llvm::Predicate::GreaterThanOrEqual);
+        builder.build_cond_br(cmp, br_big, Some(br_nobig));
+
+        builder.position_at_end(br_big);
+        let big = 255i64.compile(&context);
+        builder.build_store(big, newpx);
+        builder.build_br(br_done);
+
+        builder.position_at_end(br_nobig);
+        let min = 0i64.compile(&context);
+        let cmp = builder.build_cmp(px, min, llvm::Predicate::LessThan);
+        builder.build_cond_br(cmp, br_little, Some(br_nolittle));
+
+        builder.position_at_end(br_little);
+        let tmp = 0i64.compile(&context);
+        builder.build_store(tmp, newpx);
+        builder.build_br(br_done);
+
+        builder.position_at_end(br_nolittle);
+        builder.build_store(px, newpx);
+        builder.build_br(br_done);
+
+        builder.position_at_end(br_done);
+
+        // store the new pixel
+        let px = builder.build_load(newpx);
+        let px = builder.build_trunc(px, llvm::Type::get::<i8>(&context));
+
+        let ival = builder.build_load(i);
+        let jval = builder.build_load(j);
+
+        let w = (inpt.width() as i64).compile(&context);
+        let offset = builder.build_mul(w, jval);
+        let offset = builder.build_add(offset, ival);
+
+        let ty0 = llvm::Type::get::<i8>(&context);
+        let ty1 = llvm::Type::new_pointer(ty0);
+
+        let ptr = (out.as_ptr() as u64).compile(&context);
+        let ptr = builder.build_int_to_ptr(ptr, ty1);
+
+        let ptr = builder.build_gep(ptr, &[offset]);
+        builder.build_store(px, ptr);
+        builder.build_br(jloope);
+
+        builder.position_at_end(jloope);
+        let jval = builder.build_load(j);
+        let inc = builder.build_add(jval, 1i64.compile(&context));
+        builder.build_store(inc, j);
+        builder.build_br(jloopc);
+
+        builder.position_at_end(iloope);
+        let ival = builder.build_load(i);
+        let inc = builder.build_add(ival, 1i64.compile(&context));
+        builder.build_store(inc, i);
+        builder.build_br(iloopc);
+
+        builder.position_at_end(exit);
+        builder.build_ret_void();
+    }
+
+
+    m.verify().unwrap();
+    m.optimize(3, 1000000);
+
+    println!("{:?}", m);
+
+    let opts = llvm::JitOptions { opt_level: 3 };
+    let ee = llvm::JitEngine::new(&m, opts).unwrap();
+    let f = ee.find_function("jitfunction").unwrap();
+    ee.run_function(f, &[]);
+
+    out
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -575,7 +935,9 @@ fn main() {
         Box::new(SqrtExpr { x: Box::new(t1 + t2) })
     });
 
-    let out = grad.eval();
+    let out = run_jit(&grad);
+
+    // let out = grad.eval();
 
     let ref mut fout = File::create(&Path::new("out.png")).unwrap();
     let _ = image::ImageLuma8(out).save(fout, image::PNG);
